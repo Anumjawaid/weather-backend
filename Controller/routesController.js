e43ce2269b90bdc09f2f6bf17beb0b02a7fcb0db
Anum Jawaid <JawaiAnu@corp.contoursoftware.net>
2023-01-21 16:56:44 +0500

    user functions done

--- Controller/routesController.js
+++ Controller/routesController.js
@@ -5,7 +5,7 @@ const userModule = new mongoose.model('userLogin', userSchema);
 const request = require("request");
 const bcrypt = require("bcrypt")
 const saltRounds = 10
- 
+
 // for testing if server is working or not
 route.get('/', async (req, res) => {
     res.send({ message: 'hello from server' });
@@ -15,68 +15,66 @@ const apiKey = "a355251073b74f4899d63723232001"
 
 
 
-const getWeatherDefault=()=>{
-    let locationArray=["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+const getWeatherDefault = () => {
+    let locationArray = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
     let weatherarr = [];
-        for(let i=0;i<locationArray.length;i++){
-             // Use that city name to fetch data
-             // Use the API_KEY 
-              let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationArray[i]}&aqi=no`;
-              request(url, function(err, response, body) {
-                // On return, check the json data fetched
-                if (err) {
-                    res.render('index', { weather: null, error: 'Error, please try again' });
-                } else {
-                   
-                    let weather = JSON.parse(body);
-                    weatherarr.push({name:weather.location.name,temp:weather.current.temp_c,condition:weather.current.condition})
-                    console.log(weatherarr);
-                }
-            })
-        }
-    }
-
-
-route.post('/getWeather', function(req, res) {
-
-        // Get a single city
-        let city = req.body.city;
+    for (let i = 0; i < locationArray.length; i++) {
         // Use that city name to fetch data
         // Use the API_KEY 
-        let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
-        request(url, function(err, response, body) {
-    
+        let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationArray[i]}&aqi=no`;
+        request(url, function (err, response, body) {
             // On return, check the json data fetched
             if (err) {
                 res.render('index', { weather: null, error: 'Error, please try again' });
             } else {
+
                 let weather = JSON.parse(body);
-                console.log(weather);
+                weatherarr.push({ name: weather.location.name, temp: weather.current.temp_c, condition: weather.current.condition })
+                console.log(weatherarr);
             }
         })
-     
-    
-    });
+    }
+}
+
+
+route.post('/getWeather', function (req, res) {
+
+    // Get a single city
+    let city = req.body.city;
+    // Use that city name to fetch data
+    // Use the API_KEY 
+    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+    request(url, function (err, response, body) {
+
+        // On return, check the json data fetched
+        if (err) {
+            res.render('index', { weather: null, error: 'Error, please try again' });
+        } else {
+            let weather = JSON.parse(body);
+            console.log(weather);
+        }
+    })
+
+
+});
 
 
 
 
 //  for adding user
 route.post('/addUser', async (req, res) => {
-    console.log(req.body,"req")
-    let cities=await GetDefaultCities()
 
     const addActivity = new userModule({
         firstName: req.body.data.firstName,
         lastName: req.body.data.lastName,
         email: req.body.data.email,
-        password:await bcrypt.hash(req.body.data.password, saltRounds),
-        cities:cities
-       
+        password: await bcrypt.hash(req.body.data.password, saltRounds),
+        cities: ['karachi','Lahore','Islamabad','Quetta','Peshawar']
+
     });
 
     const result = await addActivity.save();
-    res.send({ message: 'data added' });
+    res.send({ message: result });
 });
 
 
@@ -84,29 +82,41 @@ route.post('/addUser', async (req, res) => {
 // for querying user
 
 route.post('/queryUser', async (req, res) => {
-    console.log(req.body,"req")
 
-   const result = await userModule.find({email: req.body.email})
-   console.log(result)
-    res.send({ message: 'data added' });
+    const result = await userModule.find({ email: req.body.data.email })
+    if (result.length != 0) {
+        let valid = await validateUser(req.body.data.password, result[0].password)
+        res.send({ message: valid });
+    }
+    else {
+        res.send({ message: "User doesnot exist" });
+    }
+
 });
 
-async function GetDefaultCities(){
-    return [
-        {name:"karachi",condition:{"gfgdf":"ffdfg"}},
-        {name:"Islamad",condition:{"gfgdf":"ffdfg"}},
-        {name:"peshawar",condition:{"gfgdf":"ffdfg"}}
-]
-}
 
 
-function validateUser(hash) {
 
-    bcrypt
-      .compare(password, hash)
-      .then(res => {
-        console.log(res) // return true
-      })
-      .catch(err => console.error(err.message))        
+async function validateUser(password, hash) {
+    let ret = false
+
+    await bcrypt
+        .compare(password, hash)
+        .then(res => {
+            if (res) {
+                ret = true
+            }
+            else {
+                ret = false
+            }
+            return res
+        })
+        .catch(err => {
+            ret = false
+            return false
+        }
+        )
+
+    return ret
 }
 module.exports = route;
