dc7e1c1a6b7e70c1552eeb3a99b70da6f2269264
Anum Jawaid <JawaiAnu@corp.contoursoftware.net>
2023-01-21 11:29:21 +0500

    starting

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
 mongoose.set("strictQuery", false);
+
+mongoose.connect(`mongodb+srv://Anum:anum@cluster0.x9rxwjh.mongodb.net/wheather`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('connection successful..');
+}).catch((err) => console.log(err)); 
 app.use('/', routes);
 
 app.listen(process.env.PORT || 3001, () => {
