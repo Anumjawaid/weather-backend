948db6823326c83141a6eb295c1d04128ca345a3
Insia Taha <98538044+InsiaTaha@users.noreply.github.com>
2023-01-22 11:33:25 +0500

    weather

--- Controller/routesController.js
+++ Controller/routesController.js
@@ -6,54 +6,58 @@ const routedata = require('./dataController.js');
 const request = require("request");
 const bcrypt = require("bcrypt")
 const saltRounds = 10
-const apiKey = "a355251073b74f4899d63723232001"
 
-// routedata.getWeatherDefault();
+routedata.getWeatherDefault();
 // for testing if server is working or not
-// routedata.CheckMaster('Karachi')
-route.post('/', async (req, res) => {
-    let a=await routedata.SingleRoute('khuzdarhhh')
+route.get('/', async (req, res) => {
+
     res.send({ message: 'hello from server' });
 
 })
+const apiKey = "a355251073b74f4899d63723232001"
+// route.get('/ff',routedata.test)
 
 
-// Prerequisite city name and logged in user id format req.body.data.soso
-route.post('/addCity', async (req, res) => {
-    const result = await userModule.findById(req.body.data._id ) 
-    console.log(result,"result")
-    let city=result.cities.find(element=>element==req.body.data.city)
-    if(city!=undefined){
-         res.send({ message: 'This City Has ALready Been In your List' });
-    }
-    else{
-    routedata.SingleRoute(req,res)
-    // res.send({ message: 'hello from server' });
-    }
+route.post('/getWeather', function(req, res) {
 
-})
-
-
-
-
+        // Get a single city
+        let city = req.body.city;
+        // Use that city name to fetch data
+        // Use the API_KEY 
+        let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+        request(url, function(err, response, body) {
+    
+            // On return, check the json data fetched
+            if (err) {
+                res.render('index', { weather: null, error: 'Error, please try again' });
+            } else {
+                let weather = JSON.parse(body);
+                console.log(weather);
+            }
+        })
+     
+    
+    });
 
 
 
 
 //  for adding user
 route.post('/addUser', async (req, res) => {
-    console.log(req.body,"rr")
+    console.log(req.body,"req")
+    let cities=await GetDefaultCities()
+
     const addActivity = new userModule({
         firstName: req.body.data.firstName,
         lastName: req.body.data.lastName,
         email: req.body.data.email,
-        password: await bcrypt.hash(req.body.data.password, saltRounds),
-        cities: ['karachi','Lahore','Islamabad','Quetta','Peshawar']
-
+        password:await bcrypt.hash(req.body.data.password, saltRounds),
+        cities:cities
+       
     });
 
     const result = await addActivity.save();
-    res.send({ message: "User Created" ,result,result});
+    res.send({ message: 'data added' });
 });
 
 
@@ -61,41 +65,29 @@ route.post('/addUser', async (req, res) => {
 // for querying user
 
 route.post('/queryUser', async (req, res) => {
+    console.log(req.body,"req")
 
-    const result = await userModule.find({ email: req.body.data.email })
-    if (result.length != 0) {
-        let valid = await validateUser(req.body.data.password, result[0].password)
-        res.send({ message: valid });
-    }
-    else {
-        res.send({ message: "User doesnot exist" });
-    }
-
+   const result = await userModule.find({email: req.body.email})
+   console.log(result)
+    res.send({ message: 'data added' });
 });
 
+async function GetDefaultCities(){
+    return [
+        {name:"karachi",condition:{"gfgdf":"ffdfg"}},
+        {name:"Islamad",condition:{"gfgdf":"ffdfg"}},
+        {name:"peshawar",condition:{"gfgdf":"ffdfg"}}
+]
+}
 
 
+function validateUser(hash) {
 
-async function validateUser(password, hash) {
-    let ret = false
-
-    await bcrypt
-        .compare(password, hash)
-        .then(res => {
-            if (res) {
-                ret = true
-            }
-            else {
-                ret = false
-            }
-            return res
-        })
-        .catch(err => {
-            ret = false
-            return false
-        }
-        )
-
-    return ret
+    bcrypt
+      .compare(password, hash)
+      .then(res => {
+        console.log(res) // return true
+      })
+      .catch(err => console.error(err.message))        
 }
 module.exports = route;
